a8e1085e093e806af61480bf8c2aa91fddfaea78
Gustavo Tonietto <tonietto@gmail.com>
2018-12-14 16:08:30 -0200

    [New] Add domain option

--- src/link-reader.js
+++ src/link-reader.js
@@ -8,16 +8,24 @@ const indexGenerator = require('./index-generator');
  * Loops thought array of urls to read and create multiple sitemaps.
  *
  * @param {Object} params
+ * @param {String} [params.domain] An optional domain string.
  * @param {String} params.filepath The filepath to generate the sitemap index (it is used to determinate the directory
  *                                 to generate the multiple sitemaps too).
- * @param {Number} params.count The number of links per sitemap.
+ * @param {any} params.count The number of links per sitemap.
  * @param {Number} params.quantity The quantity of sitemaps to generate.
  * @param {Array<String>} params.urls The list of urls to loop thought.
  * @param {String} params.message The final message to return.
  * @returns {Promise<String>} The final message.
  */
 const linkReader = async params => {
-  const {filepath, count, quantity, urls, message} = params; // TODO: remove count?
+  const {
+    domain: predefinedDomain,
+    filepath,
+    count,
+    quantity,
+    urls,
+    message,
+  } = params; // TODO: remove count?
   const perSitemap = Math.round(count / quantity);
   // Loop throught count
   let lastLink = 0;
@@ -25,8 +33,10 @@ const linkReader = async params => {
   const paths = [];
 
   for (let number = 0; number < quantity; number++) {
-    // Get domain and path using the first url
-    const domain = domainExtractor(urls[0]);
+    // Get domain and path using the first url if the predefinedDomain variable is absent
+    const domain = predefinedDomain
+      ? predefinedDomain
+      : domainExtractor(urls[0]);
     const {name, path, fullPath} = fileConfigurator({number, domain, filepath});
 
     paths.push(fullPath);
